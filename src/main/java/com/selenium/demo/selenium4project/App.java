377f2a432534a058bdfbec9bbeb64a51afad429d
shrikanth <Shrikanth@LAPTOP-OFKQCVLO.(none)>
2025-07-10 17:40:54 +0530

    first commit done today

---
+++ src/main/java/com/selenium/demo/selenium4project/App.java
@@ -0,0 +1,13 @@
+package com.selenium.demo.selenium4project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/selenium/demo/selenium4project/SeleniumTest2.java
@@ -0,0 +1,24 @@
+package com.selenium.demo.selenium4project;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SeleniumTest2 {
+	static WebDriver driver;
+	@Test
+	public void launchBrowser() throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+		driver.manage().window().maximize();
+		System.out.println(driver.getTitle());
+		Thread.sleep(Duration.ofSeconds(10));
+		driver.close();
+	}
+}
