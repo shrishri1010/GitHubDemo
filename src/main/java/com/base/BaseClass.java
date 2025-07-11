77a2f2148e154a08894d075679af8bf85759d7c7
shrikanth <Shrikanth@LAPTOP-OFKQCVLO.(none)>
2025-07-11 14:37:21 +0530

    third commit today

---
+++ src/main/java/com/base/BaseClass.java
@@ -0,0 +1,35 @@
+package com.base;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	protected WebDriver driver;
+	
+	@BeforeMethod
+	public void setUp() {
+		if(System.getProperty("browser").equalsIgnoreCase("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}else if (System.getProperty("browser").equalsIgnoreCase("Firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}else if(System.getProperty("browser").equalsIgnoreCase("IE")) {
+			WebDriverManager.iedriver().setup();
+			driver = new InternetExplorerDriver();
+		}
+		driver.manage().window().maximize();
+		driver.get(System.getProperty("url"));
+		System.out.println("test test");
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.close();
+	}
+}
--- src/test/java/com/selenium/demo/selenium4project/SeleniumTest.java
+++ src/test/java/com/selenium/demo/selenium4project/SeleniumTest.java
@@ -3,23 +3,27 @@ package com.selenium.demo.selenium4project;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.base.BaseClass;
 
-public class SeleniumTest {
-	static WebDriver driver;
+public class SeleniumTest  extends BaseClass{
+	
 	@Test
 	public void launchBrowser() throws InterruptedException {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		driver.get("https://demo.nopcommerce.com/login?returnUrl=%2F");
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
-		driver.manage().window().maximize();
 		System.out.println(driver.getTitle());
 		Thread.sleep(Duration.ofSeconds(10));
 		System.out.println("test test");
-		driver.close();
+	}
+	
+	@Test
+	public void testcase1() throws InterruptedException {
+		System.out.println("testcase 1");
+	}
+	
+	@Test
+	public void testcase2() throws InterruptedException {
+		System.out.println("testcase 2");
 	}
 }
